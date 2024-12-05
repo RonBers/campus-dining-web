8f9e492c0d45e20c108e0e256a8ce2d9cd200b77
Ron Bersabal <ron.bersabal@orangeandbronze.com>
2024-12-05 20:01:58 +0800

    removed app bar in dashboard screen to reduce redundancy

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,6 +1,5 @@
 import 'package:campus_dining_web/repositories/meal_repository.dart';
 import 'package:campus_dining_web/utils/constants/AppStyles.dart';
-import 'package:campus_dining_web/services/auth_service.dart';
 import 'package:campus_dining_web/widgets/add_meal_dialog.dart';
 import 'package:campus_dining_web/widgets/meal_card.dart';
 import 'package:flutter/material.dart';
@@ -31,108 +30,98 @@ class _DashboardState extends State<Dashboard> {
     }
 
     return Scaffold(
-        appBar: AppBar(
-          // backgroundColor: AppColors.primaryColor,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text('Dashboard', style: TextStyle(color: Colors.black)),
-              Row(
-                children: [
-                  SizedBox(
-                    width: 110,
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(vertical: 18),
-                        // backgroundColor: ,
-                        side: const BorderSide(
-                            color: AppColors.primaryColor, width: 2),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                      ),
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AddMealDialog(
-                                  onMealSaved: refreshDashBoard);
-                            });
-                      },
-                      child: const Text(
-                        "Add Meal",
-                        style: TextStyle(color: AppColors.primaryColor),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 25,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-        backgroundColor: Colors.white12,
-        body: Center(
-          child: FutureBuilder<List<Map<String, dynamic>>>(
-            future: fetchMeals(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('Error: ${snapshot.error}'),
-                );
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Center(child: Text('No meals available.'));
-              } else {
-                final meals = snapshot.data!;
-                return SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
+      backgroundColor: Colors.white12,
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchMeals(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No meals available.'));
+          } else {
+            final meals = snapshot.data!;
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
+                        const Text('Dashboard',
+                            style: TextStyle(color: Colors.black)),
                         SizedBox(
-                          height: MediaQuery.of(context).size.height,
-                          child: GridView.builder(
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: screenWidth > 1000
-                                  ? 5
-                                  : screenWidth > 600
-                                      ? 3
-                                      : 1,
-                              mainAxisSpacing: 5,
-                              crossAxisSpacing: 5,
-                              childAspectRatio: 200 / 190,
+                          width: 110,
+                          child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(vertical: 18),
+                              side: const BorderSide(
+                                  color: AppColors.primaryColor, width: 2),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
                             ),
-                            itemCount: meals.length,
-                            itemBuilder: (context, index) {
-                              final meal = meals[index];
-                              return Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 5),
-                                child: MealCard(
-                                    mealId: meal['id'] as String,
-                                    name: meal['name'] as String,
-                                    description: meal['description'] as String,
-                                    photoUrl: meal['photoUrl'] as String,
-                                    price: meal['price'].toString(),
-                                    isHidden: meal['isHidden'],
-                                    onMealUpdated: refreshDashBoard),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AddMealDialog(
+                                    onMealSaved: refreshDashBoard,
+                                  );
+                                },
                               );
                             },
+                            child: const Text(
+                              "Add Meal",
+                              style: TextStyle(color: AppColors.primaryColor),
+                            ),
                           ),
                         ),
-                        const SizedBox(height: 20),
                       ],
                     ),
-                  ),
-                );
-              }
-            },
-          ),
-        ));
+                    const SizedBox(height: 10),
+                    GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: screenWidth > 1000
+                            ? 5
+                            : screenWidth > 600
+                                ? 3
+                                : 1,
+                        mainAxisSpacing: 5,
+                        crossAxisSpacing: 5,
+                        childAspectRatio: 200 / 190,
+                      ),
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: meals.length,
+                      itemBuilder: (context, index) {
+                        final meal = meals[index];
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5),
+                          child: MealCard(
+                            mealId: meal['id'] as String,
+                            name: meal['name'] as String,
+                            description: meal['description'] as String,
+                            photoUrl: meal['photoUrl'] as String,
+                            price: meal['price'].toString(),
+                            isHidden: meal['isHidden'],
+                            onMealUpdated: refreshDashBoard,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
